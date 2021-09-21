aeb30e3410c0a1c2fe1851bbcaf6c649f16b1d24
farabimedicaboo <91043093+farabimedicaboo@users.noreply.github.com>
2021-09-21 17:56:17 +0700

    feat(add user dan absen)

--- common/models/attendance.js
+++ common/models/attendance.js
@@ -1,5 +1,97 @@
-'use strict';
+"use strict";
 
-module.exports = function(Attendance) {
+const moment = require("moment");
 
+module.exports = function (Attendance) {
+  // function hadir
+  Attendance.checkin = async function (username) {
+    const app = Attendance.app;
+    const Employee = app.models.Employee;
+    try {
+      const employee = await Employee.findOne({
+        where: { username: username },
+      });
+      if (!employee) {
+        return "Username invalid";
+      }
+
+      // Init start of day
+      const today = moment().startOf("day");
+
+      const checkAttendance = await Attendance.findOne({
+        where: {
+          and: [
+            { employeeId: employee.id },
+            { isApproved: true },
+            { createdAt: { gte: today } },
+            { createdAt: { lte: moment(today).endOf("day").toDate() } },
+          ],
+        },
+      });
+
+      if (checkAttendance) {
+        return "Sudah melakukan absen";
+      }
+
+      Attendance.create({
+        status: "hadir",
+        employeeId: employee.id,
+      });
+
+      return "Berhasil melakukan absen checkin";
+    } catch (error) {
+      return error;
+    }
+  };
+
+  Attendance.remoteMethod("checkin", {
+    accepts: { arg: "username", type: "string" },
+    returns: { arg: "message", type: "string" },
+  });
+
+  // function checkout
+  Attendance.checkout = async function (username) {
+    const app = Attendance.app;
+    const Employee = app.models.Employee;
+    try {
+      const employee = await Employee.findOne({
+        where: { username: username },
+      });
+      if (!employee) {
+        return "Username invalid";
+      }
+
+      // Init start of day
+      const today = moment().startOf("day");
+
+      const attendance = await Attendance.findOne({
+        where: {
+          and: [
+            { employeeId: employee.id },
+            { isApproved: true },
+            { createdAt: { gte: today } },
+            { createdAt: { lte: moment(today).endOf("day").toDate() } },
+          ],
+        },
+      });
+
+      if (attendance.end != undefined) {
+        return "Sudah melakukan absen checkout";
+      }
+
+      attendance.updateAttributes({
+        end: moment().format(),
+        updatedAt: moment().format(),
+      });
+
+      return "Berhasil melakukan absen checkout";
+    } catch (error) {
+      return error;
+    }
+  };
+
+  Attendance.remoteMethod("checkout", {
+    accepts: { arg: "username", type: "string" },
+    returns: { arg: "message", type: "string" },
+  });
 };
--- common/models/employee.js
+++ common/models/employee.js
@@ -1,5 +1,145 @@
-'use strict';
+"use strict";
 
-module.exports = function(Employee) {
+const moment = require("moment");
 
+module.exports = function (Employee) {
+  Employee.validatesUniquenessOf("username");
+
+  Employee.laporan = function (bulan, tahun) {
+    const startOfMonth = moment(`01-${bulan}-${tahun}`, "DD-MM-YYYY").utc();
+    const endOfMonth = moment(`01-${bulan}-${tahun}`, "DD-MM-YYYY")
+      .endOf("month")
+      .utc();
+
+    return Employee.aggregate([
+      {
+        $lookup: {
+          from: "Attendance",
+          let: { employeeId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $and: [
+                  { $expr: { $eq: ["$employeeId", "$$employeeId"] } },
+                  { status: "hadir" },
+                  { isApproved: true },
+                  {
+                    createdAt: {
+                      $gte: startOfMonth.toDate(),
+                      $lte: endOfMonth.toDate(),
+                    },
+                  },
+                ],
+              },
+            },
+            {
+              $project: {
+                status: 1,
+                start: {
+                  $dateToString: {
+                    date: { $add: ["$start", 7 * 60 * 60 * 1000] },
+                    format: "%H:%M",
+                  },
+                },
+              },
+            },
+            {
+              $match: { start: { $lte: "08:30" } },
+            },
+          ],
+          as: "hadir",
+        },
+      },
+      {
+        $lookup: {
+          from: "Attendance",
+          let: { employeeId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$employeeId", "$$employeeId"] },
+                isApproved: true,
+                status: "izin",
+              },
+            },
+          ],
+          as: "izin",
+        },
+      },
+      {
+        $lookup: {
+          from: "Attendance",
+          let: { employeeId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: { $eq: ["$employeeId", "$$employeeId"] },
+                isApproved: true,
+                status: "cuti",
+              },
+            },
+          ],
+          as: "cuti",
+        },
+      },
+      {
+        $lookup: {
+          from: "Attendance",
+          let: { employeeId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $and: [
+                  { $expr: { $eq: ["$employeeId", "$$employeeId"] } },
+                  { status: "hadir" },
+                ],
+              },
+            },
+            {
+              $project: {
+                start: {
+                  $dateToString: {
+                    date: { $add: ["$start", 7 * 60 * 60 * 1000] },
+                    format: "%H:%M",
+                  },
+                },
+              },
+            },
+            {
+              $match: { start: { $gt: "08:30", $lte: "24:00" } },
+            },
+          ],
+          as: "telat",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          username: 1,
+          name: 1,
+          absensi: {
+            hadir: { $size: "$hadir" },
+            telat: { $size: "$telat" },
+            izin: { $size: "$izin" },
+            cuti: { $size: "$cuti" },
+          },
+        },
+      },
+    ]);
+  };
+
+  Employee.remoteMethod("laporan", {
+    http: {
+      path: "/laporan",
+      verb: "get",
+    },
+    accepts: [
+      { arg: "bulan", type: "number", required: true },
+      { arg: "tahun", type: "number", required: true },
+    ],
+    returns: {
+      arg: "data",
+      type: "string",
+    },
+  });
 };
